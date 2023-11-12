7570152f25dd6db330bfdacc6dfd164cd8a6dbcb
Madcodes831 <r.medina2119@gmail.com>
2023-11-11 18:22:56 -0800

    All middleware and end points set up

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const{ logger } = require('./middleware/middleware')
 const server = express();
 const userRouter = require('./users/users-router');
+
 server.use(express.json());
 // remember express by default cannot parse JSON in request bodies
 server.use(logger)
@@ -12,8 +13,4 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-server.use((error, req, res, next) => { // eslint-disable-line
-  // here you could inspect the error and decide how to respond
-  res.status(error.status || 500).json({ message: error.message });
-});
 module.exports = server;
